74e44b25e48c4d82e5b774156928b92b7904b648
Leander Opitz <hello@leanderopitz.com>
2026-08-20 09:08:36 +0200

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.java
@@ -0,0 +1,15 @@
+// Repetition 2
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int key = target - nums[i];
+            if (map.containsKey(key)) {
+                return new int[] {map.get(key), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
